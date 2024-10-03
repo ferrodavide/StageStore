b0267aebf7916cb6b952cc4459e49a79173d53e9
ferrodavide <890710@stud.unive.it>
2024-10-03 11:06:30 +0200

    Grod

--- progetto/include/Node.hpp
+++ progetto/include/Node.hpp
@@ -6,27 +6,27 @@
 
 class Node {
 public:
-    // Costruttore per creare un nodo con un id e opzionalmente delle proprietà
+    // Costruttore per creare un nodo con un id passato
     Node(const std::string& id);
 
-    // Metodo per ottenere l'ID del nodo
+    // Metodo per ottenere l'id del nodo
     std::string get_id() const;
 
     // Metodo per aggiungere una proprietà al nodo
     void add_property(const std::string& key, const std::string& value);
 
-    // Metodo per ottenere il valore di una proprietà
+    // Metodo per ottenere il valore di una proprietà di un nodo, data la sua key
     std::string get_property(const std::string& key) const;
 
-    // Metodo per rimuovere una proprietà
+    // Metodo per rimuovere una proprietà di un nodo, data la sua key
     void remove_property(const std::string& key);
 
-    // Metodo per stampare tutte le proprietà del nodo (utile per il debugging)
+    // Metodo per stampare tutte le proprietà del nodo
     void print_properties() const;
 
 private:
     std::string id;  // Identificatore univoco del nodo
-    std::unordered_map<std::string, std::string> properties;  // Mappa chiave-valore per memorizzare le proprietà
+    std::unordered_map<std::string, std::string> properties;  // Unordered_map chiave-valore per memorizzare le proprietà
 };
 
-#endif // NODE_H
+#endif 
